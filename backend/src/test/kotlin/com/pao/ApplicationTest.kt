b56d2150cf64488f000d0b3a57f7e01ce0fb351a
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2024-05-29 10:38:05 -0300

    starting tests, still in progress

--- backend/src/test/kotlin/com/pao/ApplicationTest.kt
+++ backend/src/test/kotlin/com/pao/ApplicationTest.kt
@@ -3,9 +3,11 @@ package com.pao
 import com.pao.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
+import io.mockk.*
 
 import com.pao.repositories.Repo
 import com.pao.authentication.JwtService
@@ -13,16 +15,28 @@ import com.pao.authentication.hash
 
 class ApplicationTest {
     @Test
-    fun testModule() = testApplication {
+    fun testGetRandomProduct() = testApplication {
+        // Create mocks
+        val mockDb = mockk<Repo>()
+        val mockJwtService = mockk<JwtService>()
+        val mockHashFunction = mockk<(String) -> String>()
+
+        // Define behavior for mocks
+        every { mockHashFunction(any()) } returns "hashedValue"
+        // You can define other mock behaviors as needed
+
+        // Use the mocks in your application module
         application {
             module(
-                db = Repo(),
-                jwtService = JwtService(),
-                hashFunction = { s: String -> hash(s) }
+                db = mockDb,
+                jwtService = mockJwtService,
+                hashFunction = mockHashFunction
             )
         }
 
         // Testar /product/random (GET)
-
+        handleRequest(HttpMethod.Get, "/product/random").apply {
+            assertEquals(HttpStatusCode.OK, response.status())
+        }
     }
 }
