5aeebddda2b828174565b8b4f4ed7ac5918f315b
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2024-06-07 10:35:41 -0300

    initial tests done

--- backend/src/test/kotlin/com/pao/ApplicationTest.kt
+++ backend/src/test/kotlin/com/pao/ApplicationTest.kt
@@ -3,63 +3,79 @@ package com.pao
 import com.auth0.jwt.JWTVerifier
 import com.pao.authentication.JwtService
 import com.pao.repositories.Repo
-import io.ktor.client.request.*
 
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-
 import io.mockk.mockk
 import io.mockk.every
 
 import com.pao.routes.randomProduct
+import com.pao.routes.products
 import com.pao.routes.getProduct
 import io.ktor.server.routing.*
 
 import com.pao.authentication.isEmail
+import com.pao.data.classes.Product
+
+import kotlinx.serialization.json.*
 
 class ApplicationTest {
-    // Mock the dependencies
     private val mockRepo = mockk<Repo>()
     private val mockJwtService = mockk<JwtService>()
-    private val mockHashFunction: (String) -> String = { it } // Simple identity function for hashing
+    private val mockHashFunction: (String) -> String = { it }
 
     private val mockJwtVerifier = mockk<JWTVerifier>()
 
 
     @Test
-    fun testRandomProduct() {
+    fun testProducts() {
         every { mockJwtService.getVerifier() } returns mockJwtVerifier
 
-        // Run the test application with the mocked dependencies
         withTestApplication({
             module(mockRepo, mockJwtService, mockHashFunction)
             routing {
                 randomProduct()
+                getProduct()
             }
         }) {
-            // Perform a test request
             handleRequest(HttpMethod.Get, "/product/random").apply {
-                // Assert the response
                 assertEquals(HttpStatusCode.OK, response.status())
-//                assertEquals("Expected response", response.content)
-                println(response.content)
+
+                assertEquals(
+                    ContentType.Application.Json,
+                    response.contentType()
+                )
+
+                val product = Json.decodeFromString<Product>(response.content!!)
+
+                // Verificar se tem os campos da classe Product
+                assertNotNull(product.id)
+                assertNotNull(product.nome)
+                assertNotNull(product.preco)
+                assertNotNull(product.estoque)
+                assertNotNull(product.desconto)
+            }
+
+            handleRequest(HttpMethod.Get, "/product/2").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                assertEquals(
+                    ContentType.Application.Json,
+                    response.contentType()
+                )
+
+                val product = Json.decodeFromString<Product>(response.content!!)
+
+                // Verificar se tem os campos da classe Product
+                assertNotNull(product.id)
+                assertNotNull(product.nome)
+                assertNotNull(product.preco)
+                assertNotNull(product.estoque)
+                assertNotNull(product.desconto)
             }
         }
-
-//        // Make the request
-//        val response = client.get("http://127.0.0.1:8000/product/random")
-//        assertEquals(HttpStatusCode.OK, response.status)
-        // Printa o corpo da resposta
-//        println(response.body<String>())
-//        // Converte de JSON para um objeto
-//        val product = response.body<Product>()
-//        assertNotNull(product)
-//        // Verifica se o objeto é um produto listado em products
-//        assertTrue(products.contains(product))
     }
 
     @Test
