b5b7f59dbd897bb1404e3249c2c3a33db4175d2f
Eduardo Cruz Guedes <eduardo9cruz@gmail.com>
2024-06-24 13:50:00 -0300

    delete user request rota no backend

--- frontend/app/src/main/java/com/mobile/paozim/activities/profile/AccountSettingsActivity.kt
+++ frontend/app/src/main/java/com/mobile/paozim/activities/profile/AccountSettingsActivity.kt
@@ -2,15 +2,33 @@ package com.mobile.paozim.activities.profile
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mobile.paozim.R
 import com.mobile.paozim.activities.LoginActivity
 import com.mobile.paozim.activities.TabActivity
+import com.mobile.paozim.classes.UserStuff.DeleteRequest
 import com.mobile.paozim.classes.UserStuff.UserInstance
 import com.mobile.paozim.databinding.ActivityAccountSettingsBinding
+import com.mobile.paozim.retrofit.RetrofitInstance
+import com.mobile.paozim.retrofit.UserAPI
 
 class AccountSettingsActivity: AppCompatActivity() {
     private lateinit var binding: ActivityAccountSettingsBinding
+    val retIn = RetrofitInstance.getRetrofitInstance().create(UserAPI::class.java)
+
+    private fun checkPassword(pass: String): Boolean{
+        var error: String? = null
+        if(pass.isEmpty()){
+            error = "Coloque a senha"
+        } else if(pass.length < 6 || pass.length > 15){
+            error = "Senha inválida"
+        }
+        if(error != null){
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+        }
+        return error == null
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
@@ -34,21 +52,32 @@ class AccountSettingsActivity: AppCompatActivity() {
         }
 
         binding.btnDeleteAccount.setOnClickListener() {
-            // TODO - apagar usuário do banco de dados
-            UserInstance.clearUser(this)
-            val i = Intent(this, LoginActivity::class.java)
-            startActivity(i)
+            // Fazer a requisição para deletar a conta
+            // TODO - pegar senha do usuário
+            val password = binding.etPassword.text.toString()
+            if (checkPassword(password)) {
+                Toast.makeText(this, "Digite a senha para deletar a conta", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val userToDelete = DeleteRequest(UserInstance.Usuario.email, "senha")
+            retIn.deleteUser(userToDelete).enqueue(object : retrofit2.Callback<Void> {
+                override fun onFailure(call: retrofit2.Call<Void>, t: Throwable) {
+                    Toast.makeText(this@AccountSettingsActivity, "Erro ao deletar a conta", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onResponse(call: retrofit2.Call<Void>, response: retrofit2.Response<Void>) {
+                    // Se der certo, limpar o UserInstance e voltar para a tela de login
+                    UserInstance.clearUser(this@AccountSettingsActivity)
+                    val i = Intent(this@AccountSettingsActivity, LoginActivity::class.java)
+                    startActivity(i)
+                }
+            })
         }
 
         val etEmail = binding.etEmail
         val etName = binding.etName
 
-        if (UserInstance.logged) {
-            etEmail.text = UserInstance.Usuario.email
-            etName.text = UserInstance.Usuario.nome
-        } else {
-            etEmail.text = "convidado@convidado.com"
-            etName.text = "Convidado"
-        }
+        etEmail.text = UserInstance.Usuario.email
+        etName.text = UserInstance.Usuario.nome
     }
 }
--- frontend/app/src/main/java/com/mobile/paozim/retrofit/UserAPI.kt
+++ frontend/app/src/main/java/com/mobile/paozim/retrofit/UserAPI.kt
@@ -5,6 +5,7 @@ import com.mobile.paozim.classes.UserStuff.LoginRequest
 import com.mobile.paozim.classes.Responses.SimpleResponse
 import com.mobile.paozim.classes.UserStuff.User
 import com.mobile.paozim.classes.Responses.UserResponse
+import com.mobile.paozim.classes.UserStuff.DeleteRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
@@ -13,6 +14,9 @@ import retrofit2.http.POST
 const val USERS = "$API_VERSION/users"
 const val REGISTER_REQUEST = "$USERS/register"
 const val LOGIN_REQUEST = "$USERS/login"
+const val CHANGE_NAME_REQUEST = "$USERS/changeName"
+const val DELETE_REQUEST = "$USERS/delete"
+
 interface UserAPI {
     @Headers("Content-Type: application/json")
     @POST(REGISTER_REQUEST)
@@ -24,6 +28,10 @@ interface UserAPI {
 
     // Change name
     @Headers("Content-Type: application/json")
-    @POST("$USERS/changeName")
+    @POST(CHANGE_NAME_REQUEST)
     fun changeName(@Body user: ChangeNameRequest): Call<SimpleResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST(DELETE_REQUEST)
+    fun deleteUser(@Body user: DeleteRequest): Call<Void>
 }
